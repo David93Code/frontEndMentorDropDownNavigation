7b690e8aaf4f69571db68145948c3fa580801a0c
David Armeni <david.armeni1993@gmail.com>
2022-08-17 11:38:42 +0300

    some more tries

--- resources/app.js
+++ resources/app.js
@@ -7,8 +7,6 @@ function showBurgerMenu() {
     document.getElementById('container').style.position = "absolute";
     document.getElementById('container').style.top = 0;
     document.getElementById('container').style.left = 0;
-    
-
 }
 
 // need to add a div next to the nav div and set its color to transparent black rgb(0, 0, 0, .5)
@@ -24,27 +22,76 @@ function hideBurgerMenu() {
     document.getElementById('features-down').style.visibility = "hidden";
     document.getElementById('company-up').style.visibility = "hidden";
    document.getElementById('company-down').style.visibility = "hidden";
+   
 }
 
-  function showSubMenuFeatures() {
-      document.getElementById('sub-menu-features').style.visibility = "visible";
-      document.getElementById('sub-menu-features').style.maxHeight = "initial";
-    document.getElementById('features-up').style.visibility = "visible";
-     document.getElementById('features-down').style.visibility = "hidden";
-  }
+  
 
-  function hideSubMenuFeatures() {
-    document.getElementById('sub-menu-features').style.visibility = "hidden";
-    document.getElementById('sub-menu-features').style.maxHeight = "initial";
-  document.getElementById('features-up').style.visibility = "hidden";
-   document.getElementById('features-down').style.visibility = "visible";
-  }
+  const subMenuFeatures = document.getElementById('sub-menu-features');
+
+//   function showHideSubMenuFeatures() {
+//       if (subMenuFeatures.style.visibility.hidden === true) {
+//         document.getElementById('sub-menu-features').style.visibility = "visible";
+//         document.getElementById('sub-menu-features').style.maxHeight = "initial";
+//          document.getElementById('features-up').style.visibility = "visible";
+//         document.getElementById('features-down').style.visibility = "hidden";
+//      } else if (subMenuFeatures.style.visibility.hidden === false) {
+//          document.getElementById('sub-menu-features').style.visibility = "hidden";
+//           document.getElementById('sub-menu-features').style.maxHeight = "initial";
+//         document.getElementById('features-up').style.visibility = "hidden";
+//          document.getElementById('features-down').style.visibility = "visible";
+
+//      }
+//        }
+
+let featuresId = document.getElementById("features-id").classList.toggle();
+
+function showHideSubMenuFeatures() {
+    if (featuresId.isChecked() === true) {
+            document.getElementById('sub-menu-features').style.visibility = "visible";
+                 document.getElementById('sub-menu-features').style.maxHeight = "initial";
+                 document.getElementById('features-up').style.visibility = "visible";
+                 document.getElementById('features-down').style.visibility = "hidden";
+              }else if (featuresId.isChecked() === false) {
+                   document.getElementById('sub-menu-features').style.visibility = "hidden";
+                   document.getElementById('sub-menu-features').style.maxHeight = "initial";
+                   document.getElementById('features-up').style.visibility = "hidden";
+                   document.getElementById('features-down').style.visibility = "visible";
+
+              }
+}
+
+//         function showSubMenuFeatures() {
+//             if (subMenuFeatures.isChecked() === true) {
+//           document.getElementById('sub-menu-features').style.visibility = "visible";
+//           document.getElementById('sub-menu-features').style.maxHeight = "initial";
+//           document.getElementById('features-up').style.visibility = "visible";
+//          document.getElementById('features-down').style.visibility = "hidden";
+//      }
+//       }
+  
+
+//     function hideSubMenuFeatures() {
+//     if (subMenuFeatures.isChecked() === false) {
+//      document.getElementById('sub-menu-features').style.visibility = "hidden";
+//      document.getElementById('sub-menu-features').style.maxHeight = "initial";
+//      document.getElementById('features-up').style.visibility = "hidden";
+//      document.getElementById('features-down').style.visibility = "visible";
+//     }
+//    }
 
   function showSubMenuCompany() {
     document.getElementById('sub-menu-company').style.visibility = "visible" ;
     document.getElementById('sub-menu-company').style.maxHeight = "initial" ;
     document.getElementById('company-up').style.visibility = "visible";
     document.getElementById('company-down').style.visibility = "hidden";
+  }
+
+  function hideSubMenuCompany() {
+    document.getElementById('sub-menu-company').style.visibility = "hidden" ;
+    document.getElementById('sub-menu-company').style.maxHeight = "initial" ;
+    document.getElementById('company-up').style.visibility = "hidden";
+    document.getElementById('company-down').style.visibility = "visible";
   }
 
 
