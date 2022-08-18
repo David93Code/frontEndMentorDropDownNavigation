622f31c21a8e50a497f8393068e10021b23bc641
David Armeni <david.armeni1993@gmail.com>
2022-08-18 13:50:10 +0300

    js tries

--- resources/app.js
+++ resources/app.js
@@ -28,18 +28,36 @@ function hideBurgerMenu() {
 
 }
 
-function showSubMenu(btnID) {
-    console.log(btnID);
-    const subMenu= document.getElementById("sub-menu-" + btnID);
-    if (subMenu.style.visibility == "visible") {
-        subMenu.style.visibility = "hidden";
-        subMenu.style.maxHeight = "0px";
-        document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-down.svg";
-    } else {
-        subMenu.style.visibility = "visible";
-        subMenu.style.maxHeight = "fit-content";
-        document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-up.svg";
-    }
+   function showSubMenu(btnID) {
+       console.log(btnID);
+       const subMenu= document.getElementById("sub-menu-" + btnID);
+       if (subMenu.style.visibility == "visible") {
+           subMenu.style.visibility = "hidden";
+           subMenu.style.maxHeight = "0px";
+           document.getElementById("sub-menu-features").style.width = "100px";
+           document.getElementById("sub-menu-company").style.width = "100px";
+           document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-down.svg";
+       } else {
+           subMenu.style.visibility = "visible";
+           subMenu.style.maxHeight = "fit-content";
+           document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-up.svg";
+       }
    
-}
+   }
+
+//   function showSubMenu(btnID) {
+//       console.log(btnID);
+//       const subMenu= document.getElementById("sub-menu-" + btnID);
+//       if (subMenu.style.visibility == "visible") {
+//           subMenu.style.visibility = "hidden";
+//           subMenu.style.maxHeight = "0px";
+//           document.getElementById("sub-menu-features").style.width = "100px";
+//           document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-down.svg";
+//       } else {
+//           subMenu.style.visibility = "visible";
+//           subMenu.style.maxHeight = "fit-content";
+//           document.getElementById(btnID + "-down").src = "./resources/images/icon-arrow-up.svg";
+//     }
+   
+//   }
 
